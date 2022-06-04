b955d9951f30cdd3aa35d005a2b5f8f5fda75f85
BNB Hive Developer <dev@bnb-hive.net>
2022-06-04 16:37:45 +0300

    Updates

---
+++ precache-manifest.53874df2464624352d011aa83c93ecda.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "310025933616a1db8e67",
+    "url": "/static/css/main.28943890.chunk.css"
+  },
+  {
+    "revision": "310025933616a1db8e67",
+    "url": "/static/js/main.9b3af775.chunk.js"
+  },
+  {
+    "revision": "9e1cf5d6d583130239a5",
+    "url": "/static/js/runtime~main.59c14e21.js"
+  },
+  {
+    "revision": "01b1dc998f5cc2984663",
+    "url": "/static/css/2.cc56b768.chunk.css"
+  },
+  {
+    "revision": "01b1dc998f5cc2984663",
+    "url": "/static/js/2.9e7caa4e.chunk.js"
+  },
+  {
+    "revision": "f443df245d2bc7450943",
+    "url": "/static/js/3.da124fb5.chunk.js"
+  },
+  {
+    "revision": "24e41e962f8d48c51d3d",
+    "url": "/static/js/4.444384aa.chunk.js"
+  },
+  {
+    "revision": "5b651d802b640d90f9fe",
+    "url": "/static/js/5.a767ae30.chunk.js"
+  },
+  {
+    "revision": "27c0f95d16611914f0cb05a4b5f50169",
+    "url": "/index.html"
+  }
+];
---
+++ static/js/5.a767ae30.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{689:function(n,w){}}]);
+//# sourceMappingURL=5.a767ae30.chunk.js.map
